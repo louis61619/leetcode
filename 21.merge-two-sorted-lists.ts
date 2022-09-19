90ae95becb5b82d5662cf45ba13a1f4f725fb776
louis61619 <louis61619@gmail.com>
2022-09-19 17:30:37 +0800

    2022-09-19

--- 21.merge-two-sorted-lists.ts
+++ 21.merge-two-sorted-lists.ts
@@ -17,20 +17,19 @@
  * }
  */
 
-class ListNode {
-  val: number
-  next: ListNode | null
-  constructor(val?: number, next?: ListNode | null) {
-      this.val = (val===undefined ? 0 : val)
-      this.next = (next===undefined ? null : next)
-  }
-}
 
 // 鏈表節點透過 next 來查找下一個節點
 function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
   if (!list1) return list2
   if (!list2) return list1
-  if (!list1 && !list2) return null
+
+  if (list1.val > list2.val) {
+    list2.next = mergeTwoLists(list1, list2.next)
+    return list2
+  } else {
+    list1.next = mergeTwoLists(list2, list1.next)
+    return list1 
+  }
 };
 // @lc code=end
 
